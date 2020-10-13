520b342ef3313850e4f30eb91e2f5f77494923ec
Killian Murphy <killian.murphy@york.ac.uk>
2020-10-13 13:37:13 +0100

    Meet pylint criticisms

--- plot.py
+++ plot.py
@@ -1,18 +1,21 @@
+"""=============================================================================
+FILE:        plot.py
+AUTHOR:      Mat Evans <mat.evans@york.ac.uk>
+             Killian Murphy <killian.murphy@york.ac.uk>
+DESCRIPTION: Plot DSMACC model outputs for presentation to students.
+============================================================================="""
+
 import sys
 
-import matplotlib
-
-matplotlib.use("Agg")
-import pylab
-import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import pandas as pd
+import pylab
 
 data = pd.read_csv(sys.argv[1], delimiter=",", header=0)
 data.columns = data.columns.str.strip()
 for COLUMN in data.columns:
-  data[COLUMN] = pd.to_numeric(data[COLUMN])
+    data[COLUMN] = pd.to_numeric(data[COLUMN])
 
 plt.figure(1, figsize=(12, 9))
 
@@ -30,9 +33,9 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 plt.subplot(452)
 max_x = max((data["NO"] + data["NO2"]) / data["M"])
 if max_x < 1e-9:
-    factor = 1e12
+    FACTOR = 1e12
 else:
-    factor = 1e9
+    FACTOR = 1e9
 
 plt.plot(
     data["TIME"] / (24.0 * 60.0 * 60.0),
@@ -45,26 +48,26 @@ plt.plot(
         + data["HO2NO2"]
     )
     / data["M"]
-    * factor,
+    * FACTOR,
     "b",
     label="$NO_{x}$",
 )
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
-if factor == 1e12:
+if FACTOR == 1e12:
     plt.ylabel("$NO_{x} (pptv)$")
 else:
     plt.ylabel("$NO_{x} (ppbv)$")
 
 plt.plot(
     data["TIME"] / (24.0 * 60.0 * 60.0),
-    data["NO2"] / data["M"] * factor,
+    data["NO2"] / data["M"] * FACTOR,
     "g",
     label="$NO_{2}$",
 )
 plt.plot(
     data["TIME"] / (24.0 * 60.0 * 60.0),
-    data["NO"] / data["M"] * factor,
+    data["NO"] / data["M"] * FACTOR,
     "r",
     label="$NO$",
 )
@@ -77,15 +80,15 @@ plt.subplot(453)
 
 max_x = max(data["HNO3"] / data["M"])
 if max_x < 1e-9:
-    factor = 1e12
+    FACTOR = 1e12
 else:
-    factor = 1e9
+    FACTOR = 1e9
 
 
-plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["HNO3"] / data["M"] * factor)
+plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["HNO3"] / data["M"] * FACTOR)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
-if factor == 1e9:
+if FACTOR == 1e9:
     plt.ylabel("$HNO_{3} (ppbv)$")
 else:
     plt.ylabel("$HNO_{3} (pptv)$")
@@ -95,14 +98,14 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 plt.subplot(454)
 if max(data["PAN"] / data["M"]) > 1e-9:
-    factor = 1e9
+    FACTOR = 1e9
 else:
-    factor = 1e12
+    FACTOR = 1e12
 
-plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["PAN"] / data["M"] * factor)
+plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["PAN"] / data["M"] * FACTOR)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
-if factor == 1e9:
+if FACTOR == 1e9:
     plt.ylabel("$PAN (ppbv)$")
 else:
     plt.ylabel("$PAN (pptv)$")
@@ -120,24 +123,24 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(456)
-factor = 1e9
-plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C2H6"] / data["M"] * factor)
+FACTOR = 1e9
+plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C2H6"] / data["M"] * FACTOR)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
 plt.ylabel("$C_{2}H_{6} (ppbv)$")
 plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 plt.subplot(457)
-factor = 1e9
-plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C3H8"] / data["M"] * factor)
+FACTOR = 1e9
+plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C3H8"] / data["M"] * FACTOR)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
 plt.ylabel("$C_{3}H_{8} (ppbv)$")
 plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 plt.subplot(458)
-factor = 1e9
-plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C3H6"] / data["M"] * factor)
+FACTOR = 1e9
+plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C3H6"] / data["M"] * FACTOR)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
 plt.ylabel("$C_{3}H_{6} (ppbv)$")
@@ -145,7 +148,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(459)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["C5H8"] / data["M"] * 1e9)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -153,9 +156,9 @@ plt.ylabel("$Isoprene (ppbv)$")
 plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 plt.subplot(4, 5, 10)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(
-    data["TIME"] / (24.0 * 60.0 * 60.0), data["NC4H10"] / data["M"] * factor
+    data["TIME"] / (24.0 * 60.0 * 60.0), data["NC4H10"] / data["M"] * FACTOR
 )
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -164,7 +167,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 11)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["OH"] / 1e6)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -173,7 +176,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 12)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["HO2"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -182,7 +185,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 13)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["RO2"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -191,7 +194,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 14)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["NO3"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -200,7 +203,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 15)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["N2O5"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -209,7 +212,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 16)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["HCHO"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -218,7 +221,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 17)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["H2O2"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -227,7 +230,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 18)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["CH3OOH"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -236,7 +239,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 19)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(data["TIME"] / (24.0 * 60.0 * 60.0), data["CH3CHO"] / data["M"] * 1e12)
 plt.xlabel("$Time (days)$")
 plt.xticks([0, 1, 2, 3])
@@ -245,7 +248,7 @@ plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
 plt.subplot(4, 5, 20)
-factor = 1e9
+FACTOR = 1e9
 plt.plot(
     data["TIME"] / (24.0 * 60.0 * 60.0), data["CH3COCH3"] / data["M"] * 1e12
 )
@@ -255,6 +258,6 @@ plt.ylabel("($CH_{3})_{2}CO (pptv)$")
 plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=4))
 
 
-pylab.savefig(f"{sys.argv[2]}.png", format="png", bbox_inches="tight")
+pylab.savefig(sys.argv[2], format="png", bbox_inches="tight")
 
 plt.close()
