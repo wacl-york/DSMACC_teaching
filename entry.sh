6abf7a8aeabc1997e1055cad4b46bca5aef9b912
Killian Murphy <killian.murphy@york.ac.uk>
2020-10-22 17:00:03 +0100

    Update entrypoint to copy output to S3

--- entry.sh
+++ entry.sh
@@ -3,6 +3,9 @@
 SIMULATION_ID="${1}"
 S3_BUCKET_ARN="${2}"
 
-aws s3 cp "s3://${S3_BUCKET_ARN}/${SIMULATION_ID}.dat" ./ && \
-./model "${SIMULATION_ID}.dat" "${SIMULATION_ID}.csv" && \
-cat "${SIMULATION_ID}.csv"
+INPUT_FILE="${SIMULATION_ID}.dat"
+OUTPUT_FILE="${SIMULATION_ID}.csv"
+
+aws s3 cp "s3://${S3_BUCKET_ARN}/${INPUT_FILE}" ./ && \
+./model "${INPUT_FILE}" "${OUTPUT_FILE}" && \
+aws s3 cp "${OUTPUT_FILE}" "s3://${S3_BUCKET_ARN}/${OUTPUT_FILE}"
