ece84deb4a6a9c0fe35afbf27225d1c1c2a919d5
Killian Murphy <killian.murphy@york.ac.uk>
2020-11-16 20:42:19 +0000

    Get and store simulation files from object directory (#8)
    Change entry script so that it pulls initial conditions from S3 simulation directory
    and writes outputs into S3 simulation directory.

--- entry.sh
+++ entry.sh
@@ -6,6 +6,6 @@ S3_BUCKET_ARN="${2}"
 INPUT_FILE="${SIMULATION_ID}.dat"
 OUTPUT_FILE="${SIMULATION_ID}.csv"
 
-aws s3 cp "s3://${S3_BUCKET_ARN}/${INPUT_FILE}" ./ && \
+aws s3 cp "s3://${S3_BUCKET_ARN}/${SIMULATION_ID}/${INPUT_FILE}" ./ && \
 ./model "${INPUT_FILE}" "${OUTPUT_FILE}" && \
-aws s3 cp "${OUTPUT_FILE}" "s3://${S3_BUCKET_ARN}/${OUTPUT_FILE}"
+aws s3 cp "${OUTPUT_FILE}" "s3://${S3_BUCKET_ARN}/${SIMULATION_ID}/${OUTPUT_FILE}"
